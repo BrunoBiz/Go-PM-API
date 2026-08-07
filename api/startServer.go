770c2d3cc798b211ee937d113c898210d5304b76
BrunoBiz <brunobizdc@gmail.com>
2026-08-06 21:12:26 -0300

    startServer - now the log returns as a JSON parses it into a struct

--- api/startServer.go
+++ api/startServer.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -11,6 +12,16 @@ import (
 
 type serverStartRequest struct {
 	User string `json:"user" binding:"required"`
+}
+
+type serverStartOutput struct {
+	Option        string `json:"option" binding:"required"`
+	Message       string `json:"message" binding:"required"`
+	Success       bool   `json:"success" binding:"required"`
+	Status        bool   `json:"status" binding:"required"`
+	Error         string `json:"error" binding:"required"`
+	Command       string `json:"command" binding:"required"`
+	CommandResult string `json:"commandresult" binding:"required"`
 }
 
 func (server *Server) postStartServer(c *gin.Context) {
@@ -28,7 +39,6 @@ func (server *Server) postStartServer(c *gin.Context) {
 		req.User)
 
 	optStartReturn, err := server.sshClient.NewSession(commandStart)
-	slog.Info("SSH combined output: " + optStartReturn)
 
 	if err != nil {
 		slog.Error(err.Error())
@@ -36,5 +46,23 @@ func (server *Server) postStartServer(c *gin.Context) {
 		return
 	}
 
-	c.IndentedJSON(http.StatusOK, nil) // TODO - Write a proper return message - Check if already running or started and such -- Use returned message
+	// TESTING
+
+	//slog.Info("SSH combined output: \n" + optStartReturn)
+	//slog.Info(optStartReturn)
+	//fmt.Println(optStartReturn)
+
+	var serverOutput serverStartOutput
+	err = json.Unmarshal([]byte(optStartReturn[25:]), &serverOutput)
+
+	if err != nil {
+		slog.Error("Error parsing JSON", "error", err)
+	}
+	slog.Info(serverOutput.Message)
+	slog.Info(serverOutput.Option)
+	slog.Info(serverOutput.Command)
+
+	// TESTING
+
+	c.IndentedJSON(http.StatusOK, serverOutput.Message) // TODO - Write a proper return message - Check if already running or started and such -- Use returned message
 }
