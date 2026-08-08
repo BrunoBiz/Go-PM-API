4d3a9d6019a4b0ddb591c327195392e388ad077a
BrunoBiz <brunobizdc@gmail.com>
2026-08-07 23:32:36 -0300

    updog

--- api/startServer.go
+++ api/startServer.go
@@ -10,10 +10,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// API Request body
 type serverStartRequest struct {
 	User string `json:"user" binding:"required"`
 }
 
+// The output log is formatted as a JSON, which is unmarshaled into this struct
 type serverStartOutput struct {
 	Option        string `json:"option" binding:"required"`
 	Message       string `json:"message" binding:"required"`
@@ -26,7 +28,10 @@ type serverStartOutput struct {
 
 func (server *Server) postStartServer(c *gin.Context) {
 	var req serverStartRequest
+	var serverOutput serverStartOutput
 	var cntID uint64
+
+	// Parameter sent via URL
 	cntID, err := strconv.ParseUint(c.Param("id"), 10, 64)
 
 	if err := c.ShouldBindJSON(&req); err != nil {
@@ -34,35 +39,29 @@ func (server *Server) postStartServer(c *gin.Context) {
 		return
 	}
 
+	// Prepares the command to start the server
 	commandStart := fmt.Sprintf(`pct exec %d -- bash -c "su -s /bin/bash %s -c 'cd ~ && ./gameserver start'"`,
 		cntID,
 		req.User)
 
+	// Sends the command via SSH, returns the combined output - stdout + stderr
 	optStartReturn, err := server.sshClient.NewSession(commandStart)
 
 	if err != nil {
-		slog.Error(err.Error())
+		slog.Error("SSH New Session: " + err.Error())
 		c.IndentedJSON(http.StatusInternalServerError, err)
 		return
 	}
 
-	// TESTING
-
-	//slog.Info("SSH combined output: \n" + optStartReturn)
-	//slog.Info(optStartReturn)
-	//fmt.Println(optStartReturn)
-
-	var serverOutput serverStartOutput
+	// Unmarshalls the JSON formatted log into the struct
 	err = json.Unmarshal([]byte(optStartReturn[25:]), &serverOutput)
 
 	if err != nil {
-		slog.Error("Error parsing JSON", "error", err)
+		slog.Error("JSON Unmarshal - " + err.Error())
+		c.IndentedJSON(http.StatusInternalServerError, err)
 	}
-	slog.Info(serverOutput.Message)
-	slog.Info(serverOutput.Option)
-	slog.Info(serverOutput.Command)
 
-	// TESTING
+	fmt.Println(optStartReturn)
 
 	c.IndentedJSON(http.StatusOK, serverOutput.Message) // TODO - Write a proper return message - Check if already running or started and such -- Use returned message
 }
